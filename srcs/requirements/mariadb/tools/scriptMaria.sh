8469f463a5904de09eb851312d6b3523deae26f4
Giang <dollar_snowbell95@yahoo.com>
2026-08-27 00:12:27 +0300

    mariadb not working properly

---
+++ srcs/requirements/mariadb/tools/scriptMaria.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Initialize database if it doesn't exist
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	echo "Initializing MariaDB data directory..."
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql > /dev/null
+
+	# Run the temporary MariaDB in the background with no networking
+	# and continue with the script
+	echo "Starting temporary MariaDB for setup..."
+	mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking &
+	DB_PID=$!
+
+	# Wait for MariaDB using mysqladmin ping
+	echo "Waiting for MariaDB to be ready..."
+	while ! mariadb-admin ping --silent 2>/dev/null; do
+		# Once MariaDB is ready, ping succeeds (returns 0), and the loop exits
+		:
+	done
+
+	echo "Setting up database and users..."
+    # Root password
+    mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+    # Create database and user
+    mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+    mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+    mariadb -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+	echo "Database setup complete!"
+	
+	# Shut down the temporary MariaDB
+	kill $DB_PID
+	# Pause the script until temp MariaDB fully exits
+	wait $DB_PID
+fi
+
+echo "Starting MariaDB..."
+# Replace current shell process with the final MariaDB
+# With exec, MariaDB becomes PID 1 in the container & receives signals directly
+exec mariadbd --user=mysql --datadir=/var/lib/mysql
